69e7fd0604f62479c1547823293c0d7fe05287bb
Jordan Leturgez <jordanleturgez@gmail.com>
2026-08-13 00:12:46 -0400

    Delete Knapsack-problem-Unbounded directory

--- Knapsack-problem-Unbounded/backend/java/src/main/java/com/wolf/sales/Item.java
+++
@@ -1,3 +0,0 @@
-package com.wolf.sales;
-
-public record Item(String name, double value, double weight, double volume) {}
--- Knapsack-problem-Unbounded/backend/java/src/main/java/com/wolf/sales/OptimizeRequest.java
+++
@@ -1,5 +0,0 @@
-package com.wolf.sales;
-
-import java.util.List;
-
-public record OptimizeRequest(List<Item> items, double maxWeight, double maxVolume) {}
